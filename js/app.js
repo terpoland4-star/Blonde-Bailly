49ffe00a07fd6137de377d13fde782921e0ad9e1
Hamadine AG MOCTAR <terpoland4@gmail.com>
2026-03-27 21:14:01 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,48 @@
-console.log("Blonde Bailly App Loaded");
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-// Installer PWA
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(() => console.log("Service Worker enregistré"));
+function updateCartCount() {
+  const count = cart.length;
+  const el = document.getElementById("cart-count");
+  if(el) el.innerText = count;
 }
+
+function addToCart(name, price) {
+  cart.push({name, price});
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Produit ajouté !");
+  updateCartCount();
+}
+
+function displayCart() {
+  const container = document.getElementById("cart-items");
+  const totalEl = document.getElementById("total");
+
+  if(!container) return;
+
+  container.innerHTML = "";
+  let total = 0;
+
+  cart.forEach(item => {
+    total += item.price;
+
+    const div = document.createElement("div");
+    div.innerText = item.name + " - " + item.price + " FCFA";
+    container.appendChild(div);
+  });
+
+  totalEl.innerText = total;
+}
+
+function checkout() {
+  let message = "Commande Blonde Bailly:%0A";
+
+  cart.forEach(item => {
+    message += item.name + " - " + item.price + " FCFA%0A";
+  });
+
+  const url = "https://wa.me/2290161513820?text=" + message;
+  window.open(url, "_blank");
+}
+
+updateCartCount();
+displayCart();
